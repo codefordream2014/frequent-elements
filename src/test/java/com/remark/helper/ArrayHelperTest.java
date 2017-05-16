095e80656dde17f6b9c79dae2f788b9812f372ac
zhangzhaoyang <zhangzhaoyang@gome.com.cn>
2017-05-16 18:38:29 +0800

    Add unit test case

--- src/test/java/com/remark/helper/ArrayHelperTest.java
+++ src/test/java/com/remark/helper/ArrayHelperTest.java
@@ -1,5 +1,6 @@
 package com.remark.helper;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -11,10 +12,21 @@ public class ArrayHelperTest {
 
     @Test
     public void getMostFrequentElementsOfK() {
-        String[] a = {"aabb","test","test","test","test"};
-        System.out.println("input array: " + Arrays.toString(a));
-        Object[] result = ArrayHelper.getMostFrequentElementsOfK(a, 5);
-        System.out.println("result array:" + Arrays.toString(result));
+
+        Object[] emptyArray = {};
+        Assert.assertArrayEquals(new Object[]{},ArrayHelper.getMostFrequentElementsOfK(emptyArray, 5));
+
+        Object[] charsArray= {'0', '1', '1', '0', '2','0','0','1','5'};
+        Assert.assertArrayEquals(new Object[]{'0','1','2','5'},ArrayHelper.getMostFrequentElementsOfK(charsArray, 5));
+
+
+        Object[] strArray = {"aabb", "test", "test", "test", "test"};
+        Assert.assertArrayEquals(new Object[]{"test","aabb"},ArrayHelper.getMostFrequentElementsOfK(strArray, 10));
+
+        Object[] integerArray = {2, 4, 4, 3, 1};
+        System.out.println(Arrays.toString(ArrayHelper.getMostFrequentElementsOfK(integerArray, 10)));
+        Assert.assertArrayEquals(new Object[]{4,2,3,1},ArrayHelper.getMostFrequentElementsOfK(integerArray, 10));
+
     }
 
 }
